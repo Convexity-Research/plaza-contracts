f186c9c31826a6d86a93429369c68c41a7669cab
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-08-27 18:45:57 +0100

    test: updates distributor testing to accomodate latest changes

--- test/Distributor.t.sol
+++ test/Distributor.t.sol
@@ -2,17 +2,18 @@
 pragma solidity ^0.8.26;
 
 import "forge-std/Test.sol";
+import {Pool} from "../src/Pool.sol";
 import {Token} from "./mocks/Token.sol";
+import {Utils} from "../src/lib/Utils.sol";
 import {BondToken} from "../src/BondToken.sol";
 import {Distributor} from "../src/Distributor.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {PoolFactory} from "../src/PoolFactory.sol";
+import {TokenDeployer} from "../src/utils/TokenDeployer.sol";
 
 contract DistributorTest is Test {
   Distributor public distributor;
-  Distributor.Pool public pool;
-  address public poolAddress = address(0x100);
-
-  ERC1967Proxy private proxy;
+  Pool public _pool;
+  PoolFactory.PoolParams private params;
 
   address public user = address(0x1);
   address public sharesTokenOwner = address(0x2);
@@ -22,61 +23,55 @@ contract DistributorTest is Test {
 
   function setUp() public {
     vm.startPrank(deployer);
-    
+
+    address tokenDeployer = address(new TokenDeployer());
+    PoolFactory poolFactory = PoolFactory(Utils.deploy(address(new PoolFactory()), abi.encodeCall(PoolFactory.initialize, (governance,tokenDeployer))));
+
     // Distributor deploy
-    Distributor distImpl = new Distributor();
-    proxy = new ERC1967Proxy(address(distImpl), abi.encodeCall(distImpl.initialize, (governance)));
-    distributor = Distributor(address(proxy));
+    distributor = Distributor(Utils.deploy(address(new Distributor()), abi.encodeCall(Distributor.initialize, (governance))));
 
-    // Bond token deploy
-    BondToken bondImpl = new BondToken();
-    proxy = new ERC1967Proxy(address(bondImpl), abi.encodeCall(bondImpl.initialize, ("BondToken", "BOND", minter, governance, address(distributor))));
-    pool.bondToken = BondToken(address(proxy));
-
-    // Shares token deploy
-    pool.sharesToken = address(new Token("Circle USD", "USDC"));
     vm.stopPrank();
 
     // Create pool
     vm.startPrank(governance);
-    distributor.updatePool(poolAddress, address(pool.bondToken), pool.sharesToken);
+
+    params.fee = 0;
+    params.reserveToken = address(new Token("Wrapped ETH", "WETH"));
+    params.sharesPerToken = 50000;
+    params.distributionPeriod = 0;
+    params.couponToken = address(new Token("Circle USD", "USDC"));
+    
+    vm.stopPrank(); 
+    vm.startPrank(governance);
+    Token rToken = Token(params.reserveToken);
+
+    // Mint reserve tokens
+    rToken.mint(governance, 10000000000);
+    rToken.approve(address(poolFactory), 10000000000);
+
+    // Create pool and approve deposit amount
+    _pool = Pool(poolFactory.CreatePool(params, 10000000000, 10000, 10000));
+
+    _pool.dToken().grantRole(_pool.dToken().DISTRIBUTOR_ROLE(), address(distributor));
+    _pool.dToken().grantRole(_pool.dToken().MINTER_ROLE(), minter);
+    _pool.lToken().grantRole(_pool.lToken().MINTER_ROLE(), minter);
 
     // Set period to 1
-    pool.bondToken.increaseIndexedAssetPeriod(200);
-  }
-
-  function testUpdatePoolAsGovernance() public {
-    address _pool = address(0x101);
-
-    vm.startPrank(governance);
-    distributor.updatePool(_pool, address(0x111), address(0x222));
-    vm.stopPrank();
-
-    (BondToken bondToken, address sharesToken) = distributor.pools(_pool);
-
-    assertEq(address(bondToken), address(0x111));
-    assertEq(sharesToken, address(0x222));
-  }
-
-  function testUpdatePoolAsNonGovernance() public {
-    vm.startPrank(user);
-    vm.expectRevert();
-    distributor.updatePool(address(0x101), address(0x111), address(0x222));
-    vm.stopPrank();
+    _pool.dToken().increaseIndexedAssetPeriod(200);
   }
 
   function testClaimShares() public {
-    Token sharesToken = Token(pool.sharesToken);
-    
+    Token sharesToken = Token(_pool.couponToken());
+
     vm.startPrank(minter);
-    pool.bondToken.mint(user, 10000);
+    _pool.dToken().mint(user, 10000);
     sharesToken.mint(address(distributor), 1000);
     vm.stopPrank();
 
-    (uint256 lastUpdatedPeriod, uint256 shares) = pool.bondToken.userAssets(user);
+    (uint256 lastUpdatedPeriod, uint256 shares) = _pool.dToken().userAssets(user);
 
     vm.startPrank(governance);
-    pool.bondToken.increaseIndexedAssetPeriod(200);
+    _pool.dToken().increaseIndexedAssetPeriod(200);
     vm.stopPrank();
 
     vm.startPrank(user);
@@ -85,27 +80,27 @@ contract DistributorTest is Test {
     // vm.expectEmit(true, true, true, true);
     // emit Distributor.ClaimedShares(user, 1, 200);
 
-    pool.bondToken.transfer(address(0x24), 1);
+    _pool.dToken().transfer(address(0x24), 1);
 
-    (lastUpdatedPeriod, shares) = pool.bondToken.userAssets(user);
+    (lastUpdatedPeriod, shares) = _pool.dToken().userAssets(user);
 
-    distributor.claim(poolAddress);
+    distributor.claim(address(_pool));
     assertEq(sharesToken.balanceOf(user), 200);
     vm.stopPrank();
   }
 
   function testClaimInsufficientSharesBalance() public {
     vm.startPrank(minter);
-    pool.bondToken.mint(user, 1000);
+    _pool.dToken().mint(user, 1000);
     vm.stopPrank();
 
     vm.startPrank(governance);
-    pool.bondToken.increaseIndexedAssetPeriod(200);
+    _pool.dToken().increaseIndexedAssetPeriod(200);
     vm.stopPrank();
 
     vm.startPrank(user);
     vm.expectRevert(Distributor.NotEnoughSharesBalance.selector);
-    distributor.claim(poolAddress);
+    distributor.claim(address(_pool));
     vm.stopPrank();
   }
 
@@ -117,24 +112,24 @@ contract DistributorTest is Test {
   }
 
   function testClaimAfterMultiplePeriods() public {
-    Token sharesToken = Token(pool.sharesToken);
+    Token sharesToken = Token(_pool.couponToken());
 
     vm.startPrank(minter);
-    pool.bondToken.mint(user, 1000);
+    _pool.dToken().mint(user, 1000);
     sharesToken.mint(address(distributor), 50);
     vm.stopPrank();
 
     vm.startPrank(governance);
-    pool.bondToken.increaseIndexedAssetPeriod(100);
-    pool.bondToken.increaseIndexedAssetPeriod(200);
-    pool.bondToken.increaseIndexedAssetPeriod(300);
+    _pool.dToken().increaseIndexedAssetPeriod(100);
+    _pool.dToken().increaseIndexedAssetPeriod(200);
+    _pool.dToken().increaseIndexedAssetPeriod(300);
     vm.stopPrank();
 
     vm.startPrank(user);
     // vm.expectEmit(true, true, true, true);
     // emit distributor.ClaimedShares(user, 4, 50);
 
-    distributor.claim(poolAddress);
+    distributor.claim(address(_pool));
     vm.stopPrank();
 
     assertEq(sharesToken.balanceOf(user), 50);
