540d59dfdfe75ce7e994e7492adcd6f4a6b4b438
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-09-10 18:31:41 +0100

    chore: updates base mainnet price feed

--- src/OracleReader.sol
+++ src/OracleReader.sol
@@ -6,12 +6,16 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract OracleReader {
 
   // arbitrum sepolia
-  address private constant ETH_PRICE_FEED = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
-  address private constant WETH = 0x980B62Da83eFf3D4576C647993b0c1D7faf17c73;
+  // address private constant ETH_PRICE_FEED = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+  // address private constant WETH = 0x980B62Da83eFf3D4576C647993b0c1D7faf17c73;
 
   // arbitrum mainnet
   // address private constant ETH_PRICE_FEED = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
   // address private constant WETH = 0x82af49447d8a07e3bd95bd0d56f35241523fbab1;
+
+  // base mainnet
+  address private constant ETH_PRICE_FEED = 0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70;
+  address private constant WETH = 0x4200000000000000000000000000000000000006;
 
   error NoPriceFound();
 
