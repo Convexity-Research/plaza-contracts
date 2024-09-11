d6e5f71343fc67d5d236e0d7a3e0cb9fcf6fe992
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-09-11 15:28:28 +0100

    fix: updated deployment script for mockRouter

--- script/MockRouter.sol
+++ script/MockRouter.sol
@@ -6,9 +6,12 @@ import {Script, console} from "forge-std/Script.sol";
 import {Router} from "../src/MockRouter.sol";
 
 contract MockRouterScript is Script {
+  // Sepolia Base
+  address public constant ethPriceFeed = address(0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1);
+
   function run() public {
     vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-    new Router();
+    new Router(ethPriceFeed);
     vm.stopBroadcast();
   }
 }
