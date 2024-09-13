e17619238b605e805f1497d43b75c7cf565252c6
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-09-13 12:36:13 +0100

    fix: updates gitignore

---
+++ src/Trader.sol
@@ -0,0 +1,14 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.26;
+
+// import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+// contract Trader {
+//   ISwapRouter router;
+
+//   constructor(address _router, address _positionManager) {
+//     router = ISwapRouter(_router);
+//   }
+
+//   function swap()
+// }
