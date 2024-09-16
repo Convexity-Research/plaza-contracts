c9b6872a7e56e1329ced64c0372e4277667007a2
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-09-16 17:26:48 -0300

    test: adds merchant unit testing (wip)

---
+++ test/Merchant.t.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import "../src/Pool.sol";
+import "./mocks/Token.sol";
+import "forge-std/Test.sol";
+import "../src/Merchant.sol";
+import {Utils} from "../src/lib/Utils.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MerchantTest is Test {
+	Merchant public merchant;
+	Pool public pool;
+	Token public reserveToken;
+	Token public couponToken;
+	Token public dToken;
+	Token public lToken;
+
+	address public constant governance = address(0x1);
+  address public constant ethPriceFeed = address(0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70);
+
+	function setUp() public {
+		vm.startPrank(governance);
+    console.log("Deploying pool factory");
+    PoolFactory poolFactory = PoolFactory(
+      Utils.deploy(address(new PoolFactory()), 
+      abi.encodeCall(PoolFactory.initialize,
+      (governance,governance, address(0), ethPriceFeed)))
+    );
+
+    console.log("Deploying pool");
+
+    uint256 reserveAmount = 1_000_000 ether;
+    uint256 bondAmount = 25_000_000 ether;
+    uint256 leverageAmount = 1_000_000 ether;
+    uint256 sharesPerToken = 2_500_000;
+    uint256 distributionPeriod = 7776000;
+
+    PoolFactory.PoolParams memory params = PoolFactory.PoolParams({
+      fee: 0,
+      sharesPerToken: sharesPerToken,
+      reserveToken: address(new Token("Wrapped ETH", "WETH")),
+      distributionPeriod: distributionPeriod,
+      couponToken: address(new Token("Circle USD", "USDC"))
+    });
+
+    console.log("Minting reserve tokens");
+    // Mint reserve tokens
+    Token(params.reserveToken).mint(governance, reserveAmount);
+    Token(params.reserveToken).approve(address(poolFactory), reserveAmount);
+    console.log("Creating pool");
+
+    // Create pool and approve deposit amount
+    pool = Pool(poolFactory.CreatePool(params, reserveAmount, bondAmount, leverageAmount));
+    console.log("Deploying merchant");
+		merchant = new Merchant(address(0x0), address(0x0), address(0x0));
+    console.log("Setting pool");
+		vm.stopPrank();
+	}
+
+	function testHasPendingOrders() public {
+		assertFalse(merchant.hasPendingOrders(address(pool)));
+
+		// Set up pool info to trigger pending orders
+		vm.warp(block.timestamp + 13 hours);
+
+		assertTrue(merchant.hasPendingOrders(address(pool)));
+	}
+
+	function testUpdateLimitOrders() public {
+		vm.expectRevert(Merchant.UpdateNotRequired.selector);
+		merchant.updateLimitOrders(address(pool));
+
+		vm.warp(block.timestamp + 13 hours);
+		merchant.updateLimitOrders(address(pool));
+
+		// Check that orders were updated
+		(address sell, address buy, uint256 price, uint256 amount, bool filled) = merchant.orders(address(pool), 0);
+		assertEq(sell, address(reserveToken));
+		assertEq(buy, address(couponToken));
+		assertTrue(price > 0);
+		assertTrue(amount > 0);
+		assertFalse(filled);
+	}
+
+	function testOrdersPriceReached() public {
+		vm.warp(block.timestamp + 13 hours);
+		merchant.updateLimitOrders(address(pool));
+
+		assertTrue(merchant.ordersPriceReached(address(pool)));
+	}
+
+	function testExecuteOrders() public {
+		vm.warp(block.timestamp + 13 hours);
+		merchant.updateLimitOrders(address(pool));
+
+		merchant.executeOrders(address(pool));
+
+		// Check that orders were executed
+		(,,,, bool filled) = merchant.orders(address(pool), 0);
+		assertTrue(filled);
+	}
+
+	function testGetLimitOrders() public {
+		Merchant.LimitOrder[] memory orders = merchant.getLimitOrders(address(pool));
+		assertEq(orders.length, 5);
+	}
+
+	function testGetCurrentPrice() public {
+		uint256 price = merchant.getCurrentPrice(address(reserveToken), address(couponToken));
+		assertEq(price, 3000000000);
+	}
+
+	function testGetDaysToPayment() view public {
+		uint8 daysToPayment = merchant.getDaysToPayment(address(pool));
+		assertEq(daysToPayment, 7);
+	}
+
+	function testGetCouponAmount() view public {
+		uint256 couponAmount = merchant.getCouponAmount(address(pool));
+		assertEq(couponAmount, 0);
+	}
+
+	function testGetPoolReserves() view public {
+		uint256 reserves = merchant.getPoolReserves(address(pool));
+		assertEq(reserves, 10000000000);
+	}
+
+	function testGetLiquidity() view public {
+		uint256 liquidity = merchant.getLiquidity(address(reserveToken), address(couponToken));
+		assertEq(liquidity, 1000000000000000000000000000000);
+	}
+
+	function testPause() public {
+		vm.prank(governance);
+		merchant.pause();
+		assertTrue(merchant.paused());
+	}
+
+	function testUnpause() public {
+		vm.startPrank(governance);
+		merchant.pause();
+		merchant.unpause();
+		vm.stopPrank();
+		assertFalse(merchant.paused());
+	}
+}
