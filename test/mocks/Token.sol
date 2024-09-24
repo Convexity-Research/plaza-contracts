f38fad3063454998757f5fcaf2cd0fb6bb450f6f
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-09-24 10:45:29 -0300

    fix: allow Token to change its decimals

--- test/mocks/Token.sol
+++ test/mocks/Token.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.26;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+  uint8 private tokenDecimals;
 
-  constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
+  constructor (string memory name, string memory symbol) ERC20(name, symbol) {
+    tokenDecimals = 18;
+  }
 
   function mint(address to, uint256 amount) public {
     _mint(to, amount);
@@ -16,6 +19,14 @@ contract Token is ERC20 {
   }
 
   function decimals() public view virtual override returns (uint8) {
-        return 18;
+    return tokenDecimals;
+  }
+
+  function setDecimals(uint8 _decimals) external {
+    if (totalSupply() > 0) {
+      revert("Cannot set decimals after minting");
     }
+    
+    tokenDecimals = _decimals;
+  }
 }
