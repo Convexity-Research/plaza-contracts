ea1b764b7a969a3af9475c2b514e70f595372a43
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-09-24 09:40:07 -0300

    chore: adds whitelisting addresses to deployment script faucet

--- script/Faucet.s.sol
+++ script/Faucet.s.sol
@@ -8,7 +8,9 @@ import {Faucet} from "../src/Faucet.sol";
 contract FaucetScript is Script {
   function run() public {
     vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-    new Faucet();
+    Faucet f = new Faucet();
+    f.addToWhitelist(address(0x11cba1EFf7a308Ac2cF6a6Ac2892ca33fabc3398));
+    f.addToWhitelist(address(0x56B0a1Ec5932f6CF6662bF85F9099365FaAf3eCd));
     vm.stopBroadcast();
   }
 }
