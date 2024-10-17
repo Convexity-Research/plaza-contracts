a1a18329fcbd02878daacee8490f35c0d2e59ba5
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-10-17 17:57:37 -0300

    feat: auction

---
+++ script/Auction.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {SortedAuction} from "../src/SortedAuction.sol";
+
+// @todo: remove - not meant for production - just for testing
+
+contract AuctionScript is Script {
+    Auction public auction;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        auction = new Auction(address(0), address(0), 100, block.timestamp + 10 days, 1000, address(0));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/Auction.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {GasMeter} from "./utils/GasMeter.sol";
+import {Auction} from "../src/Auction.sol";
+
+import {Token} from "./utils/Token.sol";
+
+contract AuctionTest is Test, GasMeter {
+  SortedAuction auction;
+  Token usdc;
+  Token weth;
+
+  address bidder = address(0x1);
+  address house = address(0x2);
+  uint256 salt = 134;
+
+  function generateRandomBetween(uint256 min, uint256 max) public returns (uint256) {
+    require(max > min, "Max must be greater than min");
+
+    // Increment salt to ensure randomness on multiple calls within the same transaction
+    salt += 1;
+
+    // Generate a random number based on entropy and limit it to the range [min, max]
+    uint256 randomValue = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, salt)));
+
+    // Scale the random value to the desired range and return
+    return (randomValue % (max - min + 1)) + min;
+  }
+
+  function setUp() public {
+    usdc = new Token("USDC", "USDC", false);
+    weth = new Token("WETH", "WETH", false);
+
+    vm.startPrank(house);
+    auction = new SortedAuction(address(usdc), address(weth), 1000000000000, block.timestamp + 10 days, 1000, house);
+    vm.stopPrank();
+  }
+
+  function testBid() public {
+    vm.startPrank(bidder);
+
+    usdc.mint(bidder, 1000000000000 ether);
+    usdc.approve(address(auction), 1000000000000 ether);
+
+    weth.mint(address(auction), 1000000000000 ether);
+
+    uint256 gas;
+    uint256 usdcBid;
+    uint256 ethBid;
+
+    for (uint256 i = 0; i < 2000; i++) {
+      gasMeterStart();
+      usdcBid = generateRandomBetween(1, 10000);
+      ethBid = generateRandomBetween(1, 10000);
+      auction.bid(usdcBid, ethBid);
+      gas = gasMeterStop();
+      console.log(usdcBid, ethBid, gas, gas * 3 * 2400 / 10**7);
+    }
+
+    // gasMeterStart();
+    // auction.bid(generateRandomBetween(1, 10000), generateRandomBetween(1, 10000));
+    // gas = gasMeterStop();
+    // console.log("Gas used last bid:", gas);
+
+    vm.stopPrank();
+
+    vm.startPrank(house);
+
+    vm.warp(block.timestamp + 10 days);
+
+    gasMeterStart();
+    auction.endAuction();
+    gas = gasMeterStop();
+    console.log("Gas used end auction:", gas, gas * 3 * 2400 / 10**7);
+    vm.stopPrank();
+  }
+}
