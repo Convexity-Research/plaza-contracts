ce0f92df0b6c46dc97e719ccdcb7abe631069f97
Vlad <vladjdk@gmail.com>
2024-10-24 16:03:02 -0400

    fix: tests and algo decimal fix

--- src/BalancerOracleAdapter.sol
+++ src/BalancerOracleAdapter.sol
@@ -5,19 +5,21 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 import {Decimals} from "./lib/Decimals.sol";
 import {IERC20} from "@balancer/contracts/interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
 import {OracleReader} from "./OracleReader.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {IVault} from "@balancer/contracts/interfaces/contracts/vault/IVault.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import {IBalancerV2WeightedPool} from "./lib/balancer/IBalancerV2WeightedPool.sol";
 import {FixedPoint} from "./lib/balancer/FixedPoint.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BalancerOracleAdapter is Initializable, PausableUpgradeable, ReentrancyGuardUpgradeable, AggregatorV3Interface, OracleReader {
+contract BalancerOracleAdapter is Initializable, OwnableUpgradeable, UUPSUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable, AggregatorV3Interface, OracleReader {
   using Decimals for uint256;
   using FixedPoint for uint256;
 
   address public poolAddress;
-  uint8 DECIMALS;
+  uint8 public DECIMALS;
 
   error PriceTooLargeForIntConversion();
 
@@ -30,10 +32,11 @@ contract BalancerOracleAdapter is Initializable, PausableUpgradeable, Reentrancy
     address _poolAddress,
     uint8 _decimals,
     address _oracleFeeds
-  ) initializer public {
+  ) initializer external {
     __OracleReader_init(_oracleFeeds);
     __ReentrancyGuard_init();
     __Pausable_init();
+    // __setOracleFeeds(_oracleFeeds);
     poolAddress = _poolAddress;
     DECIMALS = _decimals;
   }
@@ -42,11 +45,11 @@ contract BalancerOracleAdapter is Initializable, PausableUpgradeable, Reentrancy
     return DECIMALS;
   }
 
-  function description() external view returns (string memory){
+  function description() external pure returns (string memory){
     return "Balancer Pool Chainlink Adapter";
   }
 
-  function version() external view returns (uint256){
+  function version() external pure returns (uint256){
     return 1;
   }
 
@@ -54,23 +57,23 @@ contract BalancerOracleAdapter is Initializable, PausableUpgradeable, Reentrancy
     uint80 _roundId
   ) public view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
     IBalancerV2WeightedPool pool = IBalancerV2WeightedPool(poolAddress);
-    (IERC20[] memory tokens, uint256[] memory balances,) = IVault(pool.getVault()).getPoolTokens(pool.getPoolId());
+    (IERC20[] memory tokens,,) = IVault(pool.getVault()).getPoolTokens(pool.getPoolId());
     //get weights
-    uint256[] memory weights = pool.getNormalizedWeights();
-
+    uint256[] memory weights = pool.getNormalizedWeights(); // 18 dec fractions
     uint256[] memory prices = new uint256[](tokens.length);
     for(uint8 i = 0; i < tokens.length; i++) {
-      getOraclePrice(address(tokens[i]), ETH);
+      getOraclePrice(address(tokens[i]), ETH).toBaseUnit(DECIMALS); // balancer math works with 18 dec
     }
 
     uint256 fairUintETHPrice = _calculateFairUintPrice(prices, weights, pool.getInvariant(), pool.getActualSupply());
-    uint256 fairUintUSDPrice = fairUintETHPrice * getOraclePrice(ETH, USD);
+    uint256 fairUintUSDPrice = fairUintETHPrice.mulDown(getOraclePrice(ETH, USD));
 
     if (fairUintUSDPrice > (2^256>>1)-1) {
       revert PriceTooLargeForIntConversion();
     }
 
     return (uint80(0), int256(fairUintUSDPrice), block.timestamp, block.timestamp, uint80(0));
+    // return (uint80(0), int256(0), block.timestamp, block.timestamp, uint80(0));
   }
 
   function latestRoundData()
@@ -79,16 +82,28 @@ contract BalancerOracleAdapter is Initializable, PausableUpgradeable, Reentrancy
     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound){
       return getRoundData(0);
     }
-    
-  function calculateFairUintPrice(uint256[] memory prices, uint256[] memory weights, uint256 invariant, uint256 totalBPTSupply) external view returns (uint256) {
-    return _calculateFairUintPrice(prices, weights, invariant, totalBPTSupply);
+
+  function _calculateFairUintPrice(
+    uint256[] memory prices,
+    uint256[] memory weights,
+    uint256 invariant,
+    uint256 totalBPTSupply
+    ) internal view returns (uint256) {
+    uint256 priceWeightPower = FixedPoint.ONE;
+    for(uint8 i = 0; i < prices.length; i ++) {
+      priceWeightPower = priceWeightPower.mulDown(prices[i].divDown(weights[i]).powDown(weights[i]));
+    }
+    return invariant.mulDown(priceWeightPower).divDown(totalBPTSupply);
   }
 
-  function _calculateFairUintPrice(uint256[] memory prices, uint256[] memory weights, uint256 invariant, uint256 totalBPTSupply) internal view returns (uint256) {
-    uint256 priceWeightPower = 1;
-    for(uint8 i = 0; i < prices.length; i ++) {
-      priceWeightPower *= (prices[i].divDown(weights[i])).powDown(weights[i]);
-    }
-    return (invariant * priceWeightPower / totalBPTSupply).toBaseUnit(DECIMALS);
-  }
+  /**
+   * @dev Function that should revert when `msg.sender` is not authorized to upgrade the contract. Called by
+   * {upgradeTo} and {upgradeToAndCall}.
+   * @param newImplementation Address of the new implementation contract
+   */
+  function _authorizeUpgrade(address newImplementation)
+    internal
+    onlyOwner
+    override
+  {}
 }
--- test/BalancerOracleAdapter.t.sol
+++ test/BalancerOracleAdapter.t.sol
@@ -7,6 +7,8 @@ import {Decimals} from "../src/lib/Decimals.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import {FixedPoint} from "../src/lib/balancer/FixedPoint.sol";
+import {Utils} from "../src/lib/Utils.sol";
+
 
 
 contract BalancerOracleAdapterTest is Test, BalancerOracleAdapter {
@@ -23,19 +25,13 @@ contract BalancerOracleAdapterTest is Test, BalancerOracleAdapter {
    * Grants the minter and governance roles and mints initial tokens.
    */
   function setUp() public {
-    // vm.startPrank(deployer);
-    // // Deploy and initialize BondToken
-    // BalancerOracleAdapter implementation = BalancerOracleAdapter(oracleFeeds);
-
-    // // Deploy the proxy and initialize the contract through the proxy
-    // proxy = new ERC1967Proxy(address(implementation), abi.encodeCall(implementation.initialize, (poolAddress, 18, oracleFeeds)));
-
-    // // Attach the BondToken interface to the deployed proxy
-    // adapter = BalancerOracleAdapter(address(proxy));
-    // vm.stopPrank();
+    vm.startPrank(deployer);
+    // Deploy and initialize BondToken
+    adapter = BalancerOracleAdapter(Utils.deploy(address(new BalancerOracleAdapter()), abi.encodeCall(BalancerOracleAdapter.initialize, (poolAddress, 18, oracleFeed))));
+    vm.stopPrank();
   }
 
-  function testPrices() public {
+  function testPrices() view public {
     uint256[] memory prices = new uint256[](2);
     prices[0] = 3009270000000000000000;
     prices[1] = 151850000000000000000;
@@ -45,6 +41,22 @@ contract BalancerOracleAdapterTest is Test, BalancerOracleAdapter {
     uint256 invariant = 376668723340106111392035;
     uint256 totalBPTSupply = 747200595087878845066224;
 
+    console.log(_calculateFairUintPrice(prices, weights, invariant, totalBPTSupply));
+  }
+
+  function testPrices2() view public {
+    uint256[] memory prices = new uint256[](3);
+    prices[0] = 67588000000000000000000;
+    prices[1] = 999400000000000000;
+    prices[2] = 2525283200000000000000;
+    uint256[] memory weights = new uint256[](3);
+    weights[0] = 333333333333333333;
+    weights[1] = 333333333333333333;
+    weights[2] = 333333333333333334;
+    uint256 invariant = 815266927402980198479;
+    uint256 totalBPTSupply = 2175337837604951000352;
+
+    console.logBytes32(keccak256(abi.encode(uint256(keccak256("plaza.storage.OracleReader")) - 1)) & ~bytes32(uint256(0xff)));
     console.log(_calculateFairUintPrice(prices, weights, invariant, totalBPTSupply));
   }
 }
