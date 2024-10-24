24cfbe279eafae1cb98e4191dbef786caf0a4b23
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-10-24 01:14:19 -0300

    feat: pool creates auction + tests

--- test/mocks/MockPool.sol
+++ test/mocks/MockPool.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.26;
 
 import {Pool} from "../../src/Pool.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MockPool is Pool {
   uint256 time;
@@ -12,5 +13,9 @@ contract MockPool is Pool {
 
   function setTime(uint256 _time) external {
       time = _time;
+  }
+
+  function transferReserveToAuction(uint256 amount) external override {
+    IERC20(reserveToken).transfer(msg.sender, amount);
   }
 }
