9bc0131356dbaffa416f2c3f9c51363cc4cd6777
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-10-31 00:00:42 -0300

    test: adds BalancerOracleAdapter testing

--- test/BalancerOracleAdapter.t.sol
+++ test/BalancerOracleAdapter.t.sol
@@ -2,23 +2,27 @@
 pragma solidity ^0.8.26;
 
 import "forge-std/Test.sol";
-import "../src/BalancerOracleAdapter.sol";
-import {Decimals} from "../src/lib/Decimals.sol";
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
-import {FixedPoint} from "../src/lib/balancer/FixedPoint.sol";
+
 import {Utils} from "../src/lib/Utils.sol";
-
-
+import {Decimals} from "../src/lib/Decimals.sol";
+import {OracleFeeds} from "../src/OracleFeeds.sol";
+import {FixedPoint} from "../src/lib/balancer/FixedPoint.sol";
+import {BalancerOracleAdapter} from "../src/BalancerOracleAdapter.sol";
+import {IVault} from "@balancer/contracts/interfaces/contracts/vault/IVault.sol";
+import {IBalancerV2WeightedPool} from "../src/lib/balancer/IBalancerV2WeightedPool.sol";
+import {IERC20} from "@balancer/contracts/interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract BalancerOracleAdapterTest is Test, BalancerOracleAdapter {
   using Decimals for uint256;
   using FixedPoint for uint256;
   BalancerOracleAdapter private adapter;
-  ERC1967Proxy private proxy;
+
   address private poolAddr = address(0x1);
-  address private oracleFeed = address(0x2);
+  address private oracleFeed;
+  address private priceFeed = address(0x18);
   address private deployer = address(0x3);
+
   /**
    * @dev Sets up the testing environment.
    * Deploys the BondToken contract and a proxy, then initializes them.
@@ -26,12 +30,93 @@ contract BalancerOracleAdapterTest is Test, BalancerOracleAdapter {
    */
   function setUp() public {
     vm.startPrank(deployer);
+    oracleFeed = address(new OracleFeeds());
+
     // Deploy and initialize BondToken
-    adapter = BalancerOracleAdapter(Utils.deploy(address(new BalancerOracleAdapter()), abi.encodeCall(BalancerOracleAdapter.initialize, (poolAddress, 18, oracleFeed))));
+    adapter = BalancerOracleAdapter(Utils.deploy(
+      address(new BalancerOracleAdapter()),
+      abi.encodeCall(BalancerOracleAdapter.initialize, (poolAddr, 18, oracleFeed))
+    ));
+
+    OracleFeeds(oracleFeed).setPriceFeed(adapter.ETH(), adapter.USD(), priceFeed, 1 days);
+    OracleFeeds(oracleFeed).setPriceFeed(address(0x5), adapter.ETH(), priceFeed, 1 days);
+    OracleFeeds(oracleFeed).setPriceFeed(address(0x6), adapter.ETH(), priceFeed, 1 days);
     vm.stopPrank();
   }
 
-  function testPrices() view public {
+  function testLatestRoundData() public {
+    // Mock required external calls
+    vm.mockCall(
+      poolAddr,
+      abi.encodeWithSelector(IBalancerV2WeightedPool.getVault.selector),
+      abi.encode(address(0x4))
+    );
+
+    vm.mockCall(
+      address(0x4),
+      abi.encodeWithSelector(IVault.manageUserBalance.selector),
+      bytes("")
+    );
+
+    vm.mockCall(
+      poolAddr,
+      abi.encodeWithSelector(IBalancerV2WeightedPool.getPoolId.selector),
+      abi.encode(bytes32(0))
+    );
+
+    // Mock getPoolTokens call
+    IERC20[] memory tokens = new IERC20[](2);
+    tokens[0] = IERC20(address(0x5));
+    tokens[1] = IERC20(address(0x6));
+    vm.mockCall(
+      address(0x4),
+      abi.encodeWithSelector(IVault.getPoolTokens.selector),
+      abi.encode(tokens, new uint256[](2), block.timestamp)
+    );
+
+    // Mock getNormalizedWeights call
+    uint256[] memory weights = new uint256[](2);
+    weights[0] = 500000000000000000; // 0.5
+    weights[1] = 500000000000000000; // 0.5
+    vm.mockCall(
+      poolAddr,
+      abi.encodeWithSelector(IBalancerV2WeightedPool.getNormalizedWeights.selector),
+      abi.encode(weights)
+    );
+
+    // Mock latestRoundData call for oracle feed
+    vm.mockCall(
+      priceFeed,
+      abi.encodeWithSelector(AggregatorV3Interface.latestRoundData.selector),
+      abi.encode(uint80(0), int256(100 ether), uint256(0), block.timestamp, uint80(0))
+    );
+
+    // Mock decimals call
+    vm.mockCall(
+      priceFeed,
+      abi.encodeWithSelector(AggregatorV3Interface.decimals.selector),
+      abi.encode(uint8(18))
+    );
+
+    // Mock getInvariant and getActualSupply
+    vm.mockCall(
+      poolAddr,
+      abi.encodeWithSelector(IBalancerV2WeightedPool.getInvariant.selector),
+      abi.encode(1000000000000000000)
+    );
+
+    vm.mockCall(
+      poolAddr,
+      abi.encodeWithSelector(IBalancerV2WeightedPool.getActualSupply.selector),
+      abi.encode(1000000000000000000)
+    );
+
+    // Get latest round data
+    (,int256 answer,,,) = adapter.latestRoundData();
+    assertEq(answer, 19900);
+  }
+
+  function testCalculateFairUintPrice() public pure {
     uint256[] memory prices = new uint256[](2);
     prices[0] = 3009270000000000000000;
     prices[1] = 151850000000000000000;
@@ -41,22 +126,7 @@ contract BalancerOracleAdapterTest is Test, BalancerOracleAdapter {
     uint256 invariant = 376668723340106111392035;
     uint256 totalBPTSupply = 747200595087878845066224;
 
-    console.log(_calculateFairUintPrice(prices, weights, invariant, totalBPTSupply));
-  }
-
-  function testPrices2() view public {
-    uint256[] memory prices = new uint256[](3);
-    prices[0] = 67588000000000000000000;
-    prices[1] = 999400000000000000;
-    prices[2] = 2525283200000000000000;
-    uint256[] memory weights = new uint256[](3);
-    weights[0] = 333333333333333333;
-    weights[1] = 333333333333333333;
-    weights[2] = 333333333333333334;
-    uint256 invariant = 815266927402980198479;
-    uint256 totalBPTSupply = 2175337837604951000352;
-
-    console.logBytes32(keccak256(abi.encode(uint256(keccak256("plaza.storage.OracleReader")) - 1)) & ~bytes32(uint256(0xff)));
-    console.log(_calculateFairUintPrice(prices, weights, invariant, totalBPTSupply));
+    uint256 price = _calculateFairUintPrice(prices, weights, invariant, totalBPTSupply);
+    assertTrue(price > 0);
   }
 }
