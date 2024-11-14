93aa0d333c97743ca287218dfe45d4d47e307852
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-11-14 16:44:35 +0700

    fix: MockRouter oracle price convertion calcs

--- script/MockRouter.s.sol
+++ script/MockRouter.s.sol
@@ -4,14 +4,22 @@ pragma solidity ^0.8.26;
 import {Script, console} from "forge-std/Script.sol";
 
 import {Router} from "../src/MockRouter.sol";
+import {OracleFeeds} from "../src/OracleFeeds.sol";
 
 contract MockRouterScript is Script {
   // Sepolia Base
   address public constant ethPriceFeed = address(0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1);
 
+  address public constant reserveToken = address(0x13e5FB0B6534BB22cBC59Fae339dbBE0Dc906871);
+  address public constant USD = address(0);
+
   function run() public {
     vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-    new Router(ethPriceFeed);
+    
+    OracleFeeds oracleFeeds = new OracleFeeds();
+    oracleFeeds.setPriceFeed(reserveToken, USD, ethPriceFeed, 1 days);
+
+    new Router(oracleFeeds);
     vm.stopBroadcast();
   }
 }
