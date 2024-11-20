3f0c8622e94227af5ff9cde2d972ff8bd919bc11
Nelson Galdeman <nelsongaldeman@gmail.com>
2024-11-20 13:58:48 +0000

    chore: remove oracleFeeds deployment from mockrouter script

--- script/MockRouter.s.sol
+++ script/MockRouter.s.sol
@@ -8,18 +8,15 @@ import {OracleFeeds} from "../src/OracleFeeds.sol";
 
 contract MockRouterScript is Script {
   // Sepolia Base
+  address public constant oracleFeeds = address(0x0Aa37794C3F76e52b07521879bdE99233B17cB80);
   address public constant ethPriceFeed = address(0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1);
-
   address public constant reserveToken = address(0x13e5FB0B6534BB22cBC59Fae339dbBE0Dc906871);
   address public constant USD = address(0);
 
   function run() public {
     vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
     
-    OracleFeeds oracleFeeds = new OracleFeeds();
-    oracleFeeds.setPriceFeed(reserveToken, USD, ethPriceFeed, 1 days);
-
-    new Router(address(oracleFeeds));
+    new Router(oracleFeeds);
     vm.stopBroadcast();
   }
 }
